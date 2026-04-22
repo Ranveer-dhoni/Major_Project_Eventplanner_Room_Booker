f38f7d1d849bb83a697c658e338d9d172f859317
Ranveer-dhoni <ranveer.rattan@education.nsw.gov.au>
2026-04-22 10:14:20 +0000

    Fixed up some errors in jc.script

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 // JS file initialised
 console.log("Script loaded");
 
-function encyrpt(text) {
+function encrypt(text) {
     let result = "";
-    for (let i = 0; < text.length; i++) {
-        result += String.fromCharCode(text.CharCode(i) + 3);
+    for (let i = 0; i < text.length; i++) {
+        result += String.fromCharCode(text.charCodeAt(i) + 3);
     }
     return result;
 }
+
+
 
 localStorage.setItem("teacherUser", "teacher");
 localStorage.setItem("teacherPass", encrypt("password123"));
